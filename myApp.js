6c859dba6170b32492e9a112e87a187e4c914095
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2022-12-07 13:57:29 -0600

    step 3: serve static assets

--- myApp.js
+++ myApp.js
@@ -6,6 +6,10 @@ let app = express();
 //     res.send('Hello Express')
 // })
 
+// Step 3 Solution
+app.use('/public', express.static(__dirname + '/public'))
+
+// Step 2 Solution
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
 })
