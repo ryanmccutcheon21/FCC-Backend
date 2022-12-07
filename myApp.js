fc6a82c7c33a1391420af139754299b47e246dcf
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2022-12-07 13:51:30 -0600

    step 2: serve an HTML file

--- myApp.js
+++ myApp.js
@@ -1,6 +1,11 @@
 let express = require('express');
 let app = express();
 
+// Step 1 Solution
+// app.get('/', (req, res) => {
+//     res.send('Hello Express')
+// })
+
 app.get('/', (req, res) => {
-    res.send('Hello Express')
+    res.sendFile(__dirname + '/views/index.html')
 })
